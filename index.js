2a127407d0bbd3eef6ec3cf7344e392eef05763a
Kim Foukaris <kimfoukaris@outlook.com>
2018-07-16 13:27:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 const app = "I don't do much.";
+const token = '981b96f7ad9153d169fbf35f7581aa9cca646607'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
